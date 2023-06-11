90dd0eea6e9afaea6838d6632165f5b92b95724f
Lucas-Gadelha <91857879+Lucas-Gadelha@users.noreply.github.com>
2023-06-11 19:33:47 -0300

    tentando atualizar

---
+++ App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home/home';
+import './App.css';
+import Doacao from './Doacoes/doacoes';
+import Login from './login/login';
+import Voluntario from './Voluntarios/voluntario';
+import { Eventos } from './Eventos/eventos';
+import HomeInstituicao from './Home/homeInstituicao';
+import VoluntarioPaginaInstituicao from './Voluntarios/Instituicao/voluntarioInstituicao';
+import Cadastro from './Cadastro/cadastro';
+import ListaPedidos from './Voluntarios/Instituicao/listadepedidos';
+import VerDoacao from './Voluntarios/Instituicao/verDoacao';
+import Perfil from './Perfil/Perfil';
+
+
+
+
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/doacoes' element={<Doacao/>} />
+          <Route path='/seja-um-voluntario' element={<Voluntario/>} />
+          <Route path='/eventos' element={<Eventos/>} />
+
+          <Route path='/homeAdmin' element={<HomeInstituicao/>}/>
+          <Route path='/voluntarioAdmin' element={<VoluntarioPaginaInstituicao/>} />
+          <Route path='/verPedidos' element={<ListaPedidos/>} />
+          <Route path='/verDoacoes' element={<VerDoacao/>} />
+
+          <Route path='/login' element={<Login/>} />
+          <Route path='/cadastro' element={<Cadastro/>} />
+          <Route path='/perfil' element={<Perfil/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
